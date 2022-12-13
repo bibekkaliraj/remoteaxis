87f7cfb54e9d6c37857c4a0ab546dbd29f7bf32d
Bibek <bibekkaliraj111@gmail.com>
2022-12-13 11:38:52 +0545

    inital

--- src/pages/auth/Login.screen.tsx
+++ src/pages/auth/Login.screen.tsx
@@ -1,5 +1,58 @@
+ import login from "../../assets/login.png"
+// import trees from "../../assets/trees.jpg"
+
+import React from "react";
+// import { TextInput, Button, Box } from "@mantine/core";
+// import { useForm } from "@mantine/form";
+// import LoginImage from "../assets/Login.png";
+// import Bg from "../assets/bg.png";
+import { FcGoogle } from 'react-icons/fc';
+import { useToggle, upperFirst } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import {
+    TextInput,
+    PasswordInput,
+    Checkbox,
+    Anchor,
+    Paper,
+    Title,
+    Text,
+    Container,
+    Group,
+    Button,
+  } from '@mantine/core';
+
+
 export const LoginScreen = () => {
     return (
-        <div className="message">Component</div>
-    )
-}
+        <Container size={420} my={40} className="bg-black">
+          <Title
+            align="center"
+            sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+          >
+            Welcome back!
+          </Title>
+          <Text color="dimmed" size="sm" align="center" mt={5}>
+            Do not have an account yet?{' '}
+            <Anchor<'a'> href="#" size="sm" onClick={(event) => event.preventDefault()}>
+              Create account
+            </Anchor>
+          </Text>
+    
+          <Paper withBorder shadow="xl" p="xl" mt={30} radius="md" >
+            <TextInput label="Email" placeholder="yourmail@gmail.com" required />
+            <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+            <Group position="apart" mt="lg">
+              <Checkbox label="Remember me" sx={{ lineHeight: 1 }} />
+              <Anchor<'a'> onClick={(event) => event.preventDefault()} href="#" size="sm">
+                Forgot password?
+              </Anchor>
+            </Group>
+            <Button fullWidth mt="xl" >
+              Sign in
+            </Button>
+            <h1 className="text-red-300">hello world</h1>
+          </Paper>
+        </Container>
+      );
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,60 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [],
+  // purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontSize: {
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'tiny': '.875rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '5rem',
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
     },
     colors: {
       ...colors,
-      'primary': {
-        700:'#3f3fca',
-        100:'#9f9fe5',
-      }
+      primary: {
+        700: "#3f3fca",
+        100: "#9f9fe5",
+      },
     },
     extend: {},
     spacing: {
-      'none':0,
-      'xs': '8px',
-      'sm': '16px',
-      'md': '24px',
-      'lg': '32px',
-      'xl': '48px',
-      'xxl': '64px',
+      none: 0,
+      xs: "8px",
+      sm: "16px",
+      md: "24px",
+      lg: "32px",
+      xl: "48px",
+      xxl: "64px",
     },
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-  ],
-  content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-  ],
-}
+  plugins: [],
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+};
